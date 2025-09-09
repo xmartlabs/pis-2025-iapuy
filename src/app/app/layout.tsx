08854e04a5f32ddf5573ca1b347ed192b8baa2ea
bryan.salamone <bryan.salamone2016@hotmail.com>
2025-09-09 20:45:10 -0300

    feat(sidebar): finalizar primera versión
    Revisar visibilidad condicional de los items del menú
    y la URL de los links.

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,13 +1,26 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/app/components/app-sidebar";
+import { SidebarProvider, SidebarTrigger,SidebarInset } from "@/components/ui/sidebar";
+import Link from "next/link";
+import { AppSidebar } from "@/app/components/app-sidebar"
 export default function LoginLayout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider >
-        
       <AppSidebar />
-      <main style={{marginLeft:'32px'}}>
-        {children}
-      </main>
+      <SidebarInset>
+        <SidebarTrigger className="block md:hidden"/>
+        <header className="bg-background flex h-18 border-b border-sidebar-border justify-end !py-3 !pr-8">
+          
+          <Link href="/app/perfil/">
+            <div className="w-12 h-12 rounded-full bg-[#DEEBD9] flex items-center justify-center cursor-pointer">
+              <span className="font-normal text-lg leading-[100%] tracking-normal text-center align-middle">BS</span>
+            </div>
+          </Link>
+        </header>
+        
+        <main className="!ml-8 !mt-[60px]">
+          
+          {children}
+        </main>
+      </SidebarInset>
     </SidebarProvider>
   )
 }
--- src/app/components/app-sidebar.tsx
+++ src/app/components/app-sidebar.tsx
@@ -1,12 +1,12 @@
 'use client'
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import { Building,Dog,PersonStanding,CalendarRange,BadgeDollarSign} from "lucide-react"
 import Image from 'next/image'
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { inter } from "@/fonts";
 import {
   Sidebar,
   SidebarContent,
+  SidebarHeader,
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
@@ -15,56 +15,63 @@ import {
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
 
-
 // Menu items.
 const items = [
   {
     title: "Perros",
     url: "/app/admin/perros",
+    icon:Dog,
   },
   {
     title: "Personas",
     url: "/app/admin/personas",
+    icon:PersonStanding,
   },
   {
     title: "Instituciones",
     url: "/app/admin/instituciones",
+    icon:Building,
   },
   {
     title: "Intervenciones",
     url: "/app/admin/intervenciones",
+    icon:CalendarRange,
   },
   {
     title: "Gastos",
     url: "/app/admin/gastos",
+    icon:BadgeDollarSign,
   },
 ]
 
 export function AppSidebar() {
   const pathName=usePathname()
   return (
-    <Sidebar className="w-[260px]">
-      <div className="w-[258px] h-[133px]">
-          <Image
-            src="/logo.png"
-            alt="Logo"
-            width={53}
-            height={53}
-          style={{marginLeft:'24px',marginTop:'40px'}}
-          />
+    <Sidebar>
+      <SidebarHeader className=" h-[261px]">
+        <div className="w-[258px] h-[133px]">
+            <Image
+              src="/logo.png"
+              alt="Logo"
+              width={150}
+              height={150}
+            style={{marginLeft:'16px',marginTop:'48px'}}
+            />
         </div>
-      <SidebarContent>
+      </SidebarHeader>
+      <SidebarContent className="!px-4" >
         <SidebarGroup>
           <SidebarGroupContent>
-            <SidebarMenu className={`${inter.className} antialiased`}>
+            <SidebarMenu>
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton asChild>
-                    <div className={`w-[258px] h-[48px] flex items-center justify-center ${pathName === item.url ? "bg-[#E2E7F0]" : ""}`}>
+                    <div className={`w-[228px] h-[40px] flex items-center justify-start ${pathName === item.url ? "bg-[#DEEBD9]" : ""}`}>
                        <Link
                           href={item.url}
-                          className="w-[210px] h-[24px] flex items-center justify-center">
-                          <span className="font-medium text-[14px] leading-[24px] tracking-[-0.01em]">{item.title}</span>
+                          className="flex items-center gap-2">
+                          <item.icon className="mr-2"/>
+                          <span className="font-medium text-[14px] leading-5 tracking-normal">{item.title}</span>
                         </Link>
                     </div>
                   </SidebarMenuButton>
