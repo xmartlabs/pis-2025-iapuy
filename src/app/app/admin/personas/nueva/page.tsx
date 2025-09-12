c6b3f17ac4e6cc71b20f7bf760d152f7a73ddb3c
Felipe Carranza <felipe.carranza@fing.edu.uy>
2025-09-12 13:54:45 -0300

    feat(personas): comence UI de crear persona, falta estilo

---
+++ src/app/app/admin/personas/nueva/page.tsx
@@ -0,0 +1,12 @@
+import Migas from "./Migas";
+import Formulario from "./Formulario";
+
+export default function NuevaPersona() {
+  return (
+    <>
+        <Migas/>
+        <h1 className="text-6xl font-bold">Nueva persona</h1>
+        <Formulario />
+    </>
+  );
+}
