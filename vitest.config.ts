061a3abe19f5c404b3e518a0b36ee1343a8e7efd
mayyte <carromayte@gmail.com>
2025-09-19 22:14:54 -0300

    coverage de etst

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig } from "vitest/config";
+import path from "path";
 
 export default defineConfig({
-  test: {
-    include: ["src/**/*.test.ts"], // busca todos los tests
-    coverage: {
-      provider: "v8",              // cambiar "c8" por "v8"
-      reporter: ["text", "lcov"],  // mostrar en consola y generar html
-      all: true,                    // incluye todos los archivos
-      include: ["src/**/*.ts"],     // archivos a cubrir
-      // exclude opcional, si quieres incluir todo no pongas nada
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), 
     },
-    watch: true,                    // modo watch
+  },
+  test: {
+    include: ["src/**/*.test.ts"], 
+    coverage: {
+      provider: "v8",              
+      reporter: ["text", "lcov"],  
+      all: true,                    
+      include: ["src/**/*.ts"],     
+    },
+    watch: true,                    
   },
 });
