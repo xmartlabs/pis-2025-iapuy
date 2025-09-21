91c03804c274aa35cef2ffc0a21a00d1449239d3
FCarranza-F <felipe.carranza@fing.edu.uy>
2025-09-21 09:34:12 -0300

    chore: fixed merge

--- src/lib/crypto/hash.ts
+++ src/lib/crypto/hash.ts
@@ -1,19 +1,6 @@
 import bcrypt from "bcrypt";
 
 export class Hashing {
-<<<<<<< HEAD
-	// Hashear contraseña
-	static async hashPassword(password: string) {
-		const saltRounds = 10;
-		return await bcrypt.hash(password, saltRounds);
-	}
-
-	// Verificar contraseña
-	static async verifyPassword(password: string, hash: string) {
-		return await bcrypt.compare(password, hash);
-	}
-}
-=======
   // Hashear contraseña
   static async hashPassword(password: string) {
     const saltRounds = 10;
@@ -25,4 +12,3 @@ export class Hashing {
     return await bcrypt.compare(password, hash);
   }
 }
->>>>>>> dev
