c9674574da09813dad84996cc7c845cfd28abaf9
agustin.occhiuzzi <agustin.occhiuzzi@fing.edu.uy>
2025-10-02 15:39:43 -0300

    feat(evaluar) : add endpoint to find dogs from the intervention

--- src/app/api/Intervencion/service/Intervencion.service.ts
+++ src/app/api/Intervencion/service/Intervencion.service.ts
@@ -12,32 +12,43 @@ import { PerroExperiencia } from "@/app/models/perros-experiencia.entity";
 import { InstitucionIntervencion } from "@/app/models/institucion-intervenciones.entity";
 import { InstitucionPatologias } from "@/app/models/intitucion-patalogia.entity";
 import { Patologia } from "@/app/models/patologia.entity";
+import { UsrPerro } from "@/app/models/usrperro.entity";
+import { Perro } from "@/app/models/perro.entity";
 
 export class IntervencionService {
 
   async findAllPathologiesbyId(id: string) {
-    try{
         const relation = await InstitucionIntervencion.findOne({
         where: { intervencionId: id }
         });
-        if(relation){
-          const institutionId = relation.institucionId;
-          const pathologiesRelation = await InstitucionPatologias.findAll({
-            where: { institucionId : institutionId }
-          });
-          const pathologies = await Promise.all(
-            pathologiesRelation.map((rel) =>
-              Patologia.findByPk(rel.patologiaId, {
-                attributes: ["id", "nombre"]
-              })
-            )
-          );
+        if(!relation){ return [] }
+        const institutionId = relation.institucionId;
+        const pathologiesRelation = await InstitucionPatologias.findAll({
+          where: { institucionId : institutionId }
+        });
+        const pathologies = await Promise.all(
+          pathologiesRelation.map((rel) =>
+            Patologia.findByPk(rel.patologiaId, {
+              attributes: ["id", "nombre"]
+            })
+          )
+        );
+        return pathologies
+  }
 
-          return pathologies
-        }
-    }catch (error) {
-      throw error
-    }
+  async findAllDogsbyId(id: string) {
+      const pairsUserDog = await UsrPerro.findAll({
+        where: { intervencionId: id }
+      });
+      if(!pairsUserDog){ return [] }
+      const dogs = await Promise.all(
+        pairsUserDog.map((pair) =>
+          Perro.findByPk(pair.perroId, {
+            attributes: ["id", "nombre"]
+          })
+        )
+      );
+      return dogs
   }
 
   async findAll(
