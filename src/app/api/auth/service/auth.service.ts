21cb1a2015466eadf1cc73290134ad425bc0b0e5
agustin.biassini <biassini43@gmail.com>
2025-10-05 17:56:33 -0300

    fix(login): front shows appropiate error message

--- src/app/api/auth/service/auth.service.ts
+++ src/app/api/auth/service/auth.service.ts
@@ -27,12 +27,11 @@ export class AuthService {
     await initDatabase();
 
     const user = await this.userService.findOneForAuth(ci);
-    if (
-      user === undefined ||
-      user === null ||
-      !(await Hashing.verifyPassword(password, user.password))
-    ) {
-      return { error: "Credenciales inválidas", status: 401 };
+    if (!user) {
+      return { error: "Este usuario no existe", status: 401 };
+    }
+    if (!(await Hashing.verifyPassword(password, user.password))) {
+      return { error: "Credenciales inválidas", status: 402 };
     }
     const userCI = user.ci;
     const userName = user.nombre;
