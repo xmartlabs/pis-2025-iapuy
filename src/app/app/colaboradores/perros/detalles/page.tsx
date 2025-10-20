d4f8a229ed5d97c96f27745a09e9b04b55629e39
Santiago Lopez de haro <santiagolhgpro@gmail.com>
2025-10-20 17:30:38 -0300

    fix(Dogs): Collaborators won't be able to see the delete button of dogs, also the backend has protections against this

--- src/app/app/colaboradores/perros/detalles/page.tsx
+++ src/app/app/colaboradores/perros/detalles/page.tsx
@@ -3,10 +3,28 @@ import DetallePerro from "@/app/components/dogs/detalle-perro";
 import EliminarPerro from "@/app/components/dogs/eliminar-perro";
 import HistorialIntervenciones from "@/app/components/dogs/historial-intervenciones";
 import HistorialSanidad from "@/app/components/dogs/historial-sanidad";
+import { LoginContext } from "@/app/context/login-context";
 import { SanidadProvider } from "@/app/context/sanidad-context";
-import { Suspense } from "react";
+import type { JwtPayload } from "jsonwebtoken";
+import { Suspense, useContext } from "react";
 
 export default function PantallaPerros() {
+  const context = useContext(LoginContext);
+
+  const token = context?.tokenJwt;
+
+  let userType: string | null = null;
+
+  if (token) {
+    try {
+      const payloadBase64 = token.split(".")[1];
+      const payloadJson = JSON.parse(atob(payloadBase64)) as JwtPayload;
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      userType = payloadJson.type;
+    } catch {
+      reportError("Error al decodificar el token:");
+    }
+  }
   return (
     <SanidadProvider>
       <div className="w-[96%] flex flex-col gap-4 pl-2">
@@ -19,9 +37,11 @@ export default function PantallaPerros() {
         <Suspense fallback={<div>Cargando historial de sanidad...</div>}>
           <HistorialSanidad />
         </Suspense>
-        <Suspense fallback={<div>Cargando opciones...</div>}>
-          <EliminarPerro />
-        </Suspense>
+        {userType === "Administrador" && (
+          <Suspense fallback={<div>Cargando opciones...</div>}>
+            <EliminarPerro />
+          </Suspense>
+        )}
       </div>
     </SanidadProvider>
   );
