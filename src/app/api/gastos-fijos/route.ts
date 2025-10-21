fd047d07a28a90d0b9f2bf1572beccee41b8ad57
santiago lopez de haro <santiagolhgpro@gmail.com>
2025-10-21 20:39:34 -0300

    feat(Gastos fijos): Added error handling on endpoints

--- src/app/api/gastos-fijos/route.ts
+++ src/app/api/gastos-fijos/route.ts
@@ -6,15 +6,29 @@ import UpdateGastosDTO from "./dtos/update-gastos.dto";
 const gastosFijosController = new GastosFijosController();
 
 export async function GET() {
-  return NextResponse.json(gastosFijosController.getCostos());
+  try {
+    return NextResponse.json(gastosFijosController.getCostos());
+  } catch (error) {
+    return NextResponse.json(
+      error instanceof Error ? error.message : "Internal server error",
+      { status: 500 }
+    );
+  }
 }
 
 export async function PUT(request: NextRequest) {
-  const body = await request.json();
+  try {
+    const body = await request.json();
 
-  const costos = plainToInstance(UpdateGastosDTO, body, {
-    enableImplicitConversion: true,
-  });
-  await gastosFijosController.loadCostos(costos);
-  return NextResponse.json(null, { status: 201 });
+    const costos = plainToInstance(UpdateGastosDTO, body, {
+      enableImplicitConversion: true,
+    });
+    await gastosFijosController.loadCostos(costos);
+    return NextResponse.json(null, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      error instanceof Error ? error.message : "Internal server error",
+      { status: 500 }
+    );
+  }
 }
