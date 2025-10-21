0bce41aa913deea6f6c06f21dfdb53ac4fd2c5d7
Ignacio de Souza <ignacio.de.souza@fing.edu.uy>
2025-10-21 12:26:40 -0300

    feat(test): create crear-eliminar-colab integration test

---
+++ tests/usuarios/crear-eliminar-colab.ts
@@ -0,0 +1,64 @@
+
+// Lo dejo comentado porque está el bug de cédula repetida
+
+// import { test, expect } from "@playwright/test";
+
+// test("test", async ({ page }) => {
+//   await page.goto("https://slincsilver.ddns.net:3000/");
+//   await page.getByRole("textbox", { name: "Cédula de identidad" }).click();
+//   await page
+//     .getByRole("textbox", { name: "Cédula de identidad" })
+//     .fill("11111111");
+//   await page.getByRole("textbox", { name: "Contraseña" }).click();
+//   await page.getByRole("textbox", { name: "Contraseña" }).fill("password1");
+//   await page.getByRole("button", { name: "Iniciar" }).click();
+//   await page.getByRole("link", { name: "Personas" }).click();
+//   await page.locator("span").filter({ hasText: "Agregar Persona" }).click();
+//   await page.getByRole("textbox", { name: "Nombre y apellido*" }).click();
+//   await page
+//     .getByRole("textbox", { name: "Nombre y apellido*" })
+//     .fill("Tarifa Melida");
+//   await page.getByRole("radio", { name: "Colaborador" }).click();
+//   await page.getByRole("textbox", { name: "Banco*" }).click();
+//   await page.getByRole("textbox", { name: "Banco*" }).fill("BBVA");
+//   await page.getByRole("textbox", { name: "Número de cuenta*" }).click();
+//   await page
+//     .getByRole("textbox", { name: "Número de cuenta*" })
+//     .fill("UY12343212");
+//   await page.getByRole("textbox", { name: "Cédula de identidad*" }).click();
+//   await page
+//     .getByRole("textbox", { name: "Cédula de identidad*" })
+//     .fill("92971471");
+//   await page.getByRole("textbox", { name: "Cédula de identidad*" }).click();
+//   await page.getByRole("textbox", { name: "Celular*" }).click();
+//   await page.getByRole("textbox", { name: "Celular*" }).fill("091891123");
+
+//   await page.locator('[id^="_r_"][id$="-form-item"]').last().click();
+
+//   await page.getByRole("button", { name: "Crear persona" }).click();
+//   await page.getByRole("button", { name: "Copiar link", exact: true }).click();
+
+//   const link = await page.evaluate(
+//     async () => await navigator.clipboard.readText()
+//   );
+//   await page.getByRole("button", { name: "Close" }).click();
+//   await page.getByText("SS").click();
+//   await page.getByRole("menuitem", { name: "Cerrar sesión" }).click();
+//   await page.goto(link);
+
+//   await page.getByRole("textbox", { name: "Nueva contraseña" }).click();
+//   await page
+//     .getByRole("textbox", { name: "Nueva contraseña" })
+//     .fill("Contrasenia");
+//   await page.getByRole("button", { name: "Confirmar" }).click();
+
+//   await page
+//     .getByRole("textbox", { name: "Cédula de identidad" })
+//     .fill("92971471");
+//   await page.getByRole("textbox", { name: "Contraseña" }).click();
+//   await page.getByRole("textbox", { name: "Contraseña" }).fill("Contrasenia");
+//   await page.getByRole("button", { name: "Iniciar" }).click();
+//   await page.getByText("VM").click();
+//   await page.getByRole("menuitem", { name: "Mi perfil" }).click();
+//   await expect(page.getByText("VM")).toBeVisible();
+// });
