5169fc55036c4f9c4a1aa8a31e7aa59f1eb353f5
jonathan <jonarodores@gmail.com>
2025-10-25 08:11:04 -0300

    api for aoi

--- src/app/app/admin/personas/detalle/page.tsx
+++ src/app/app/admin/personas/detalle/page.tsx
@@ -19,65 +19,34 @@ function DetallePersonaContent() {
   };
 
   const fetchUser = async (): Promise<UserSanitized | null> => {
-    const resp = await fetch(`/aoi/users/${ci}`, {
+    const resp = await fetch(`/api/users/${ci}`, {
       method: "GET",
       headers: baseHeaders,
     });
     if (!resp) {
+      console.error("Error al obtener el usuario");
       return null;
     } 
+    console.error("obtenido resp");
     const respJson = await resp.json() as UserSanitized;
+    console.error("Sin Error");
     return respJson;
 };
   return (
     <div className="flex flex-col">
-      Detalle Persona CI: {ci}, nombress
+      Detalle Persona CI: {ci}, nombre {fetchUser().then(user => user?.nombre ?? 'Desconocido')}
       <BotonEliminarUsuario ci={ci} />
     </div>
   );
 }
 
+
+
 export default function DetallePersona() {
   return (
+    
     <Suspense fallback={<div>Cargando persona...</div>}>
       <DetallePersonaContent />
     </Suspense>
   );
 }
-
-/*
-How to call the GET
-
-
-const resp = fetch(`/aoi/users/${ci}`, {
-    method: "GET",
-    headers: baseHeaders,
-  });
-
-// src\app\api\users\[ci]\route.ts
-import { NextResponse, type NextRequest } from "next/server";
-import { UserController } from "../controller/user.controller";
-import { initDatabase } from "@/lib/init-database";
-const userController = new UserController();
-
-await initDatabase();
-
-export async function GET(
-  request: NextRequest,
-  { params }: { params: Promise<{ ci: string }> }
-) {
-  try {
-    const { ci } = await params;
-    const data = await userController.getUser(request, { ci });
-    return NextResponse.json(data);
-  } catch (error) {
-    if (error instanceof Error && error.message === "Usuario no encontrado") {
-      return NextResponse.json({ error: error.message }, { status: 404 });
-    }
-    return NextResponse.json(
-      { error: "Error interno del servidor" },
-      { status: 500 }
-    );
-  }
-}
-  */
