9a4d91182e02691a52a5561727f2a5578d9296ad
agustin.biassini <biassini43@gmail.com>
2025-10-26 19:53:49 -0300

    fix: added status to intervention details + button to change to suspended + backend for button

--- src/app/api/intervention/[id]/route.ts
+++ src/app/api/intervention/[id]/route.ts
@@ -3,7 +3,6 @@ import { initDatabase } from "@/lib/init-database";
 import { InterventionController } from "../controller/intervention.controller";
 import { NextRequest, NextResponse } from "next/server";
 
-
 const interventionController = new InterventionController();
 await initDatabase();
 
@@ -39,5 +38,21 @@ export async function GET(
       { error: "Internal server error" },
       { status: 500 }
     );
+  }
+}
+
+export async function POST(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  try {
+    const { id } = await params;
+    const updated = await interventionController.suspendIntervention(id);
+    return NextResponse.json(updated);
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ error: error.message }, { status: 400 });
+    }
+    return NextResponse.json({ error: String(error) }, { status: 500 });
   }
 }
