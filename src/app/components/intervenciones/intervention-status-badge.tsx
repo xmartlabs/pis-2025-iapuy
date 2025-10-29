8cb02c73e92a9ee4a1efe0c55f774bd7e43b6bc3
agustin.biassini <biassini43@gmail.com>
2025-10-29 13:00:13 -0300

    fix: added missing styles to intervention label

--- src/app/components/intervenciones/intervention-status-badge.tsx
+++ src/app/components/intervenciones/intervention-status-badge.tsx
@@ -1,23 +1,28 @@
-import { Badge } from "@/components/ui/badge"
+import { Badge } from "@/components/ui/badge";
 
-export interface Props{
-    statusType:"Pendiente de asignación"|"Cupo completo"|"Realizada"|"Finalizada"|"Suspendida";
+export interface Props {
+  statusType:
+    | "Pendiente de asignación"
+    | "Cupo completo"
+    | "Realizada"
+    | "Finalizada"
+    | "Suspendida";
 }
 
-export default function InterventionBadge({statusType}:Props) {
-    const statusStyles: Record<Props["statusType"], string> = {
-        "Pendiente de asignación": "bg-[#FECACA]",
-        "Cupo completo": "bg-[#FDE68A]",
-        Realizada: "bg-[#BAE6FD]",
-        Finalizada: "bg-[#DEEBD9]",
-        Suspendida: "bg-[#D4D4D4]"
-    };
-    const className = statusStyles[statusType]
-    return (
-        <Badge 
-        className={`${className} !font-sans !font-semibold !text-xs !leading-4 !text-[#1B2F13]`}        
-        >
-            {statusType}
-        </Badge>
-    )
+export default function InterventionBadge({ statusType }: Props) {
+  const statusStyles: Record<Props["statusType"], string> = {
+    "Pendiente de asignación": "bg-[#FECACA]",
+    "Cupo completo": "bg-[#FDE68A]",
+    Realizada: "bg-[#BAE6FD]",
+    Finalizada: "bg-[#DEEBD9]",
+    Suspendida: "bg-[#D4D4D4]",
+  };
+  const className = statusStyles[statusType];
+  return (
+    <Badge
+      className={`${className} !font-sans !font-semibold !text-xs !leading-4 !text-[#1B2F13] !rounded-full !py-0.5 !px-2.5`}
+    >
+      {statusType}
+    </Badge>
+  );
 }
