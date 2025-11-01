d08854d73eea8f0e8cdc450d0a52b3e7aa0a467e
santiago lopez de haro <santiagolhgpro@gmail.com>
2025-11-01 12:00:35 -0300

    fix: Remove unecesarry check

--- src/app/api/instituciones/[id]/route.ts
+++ src/app/api/instituciones/[id]/route.ts
@@ -4,16 +4,9 @@ import { initDatabase } from "@/lib/init-database";
 import { InstitucionesController } from "../controller/institucion.controller";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-import jwt from "jsonwebtoken";
 
 const institutionsController = new InstitucionesController();
 await initDatabase();
-
-interface PayloadForUser extends jwt.JwtPayload {
-  ci: string;
-  name: string;
-  type: string;
-}
 
 export async function GET(
   request: NextRequest,
@@ -43,27 +36,10 @@ export async function PUT(
   const { id } = await params;
 
   try {
-    // Verificar que el usuario es administrador
-    const authHeader = request.headers.get("authorization") ?? "";
-    const accessToken = authHeader.split(" ")[1];
-
-    if (!accessToken) {
-      return NextResponse.json(
-        { error: "No se encontró un token de acceso en la solicitud." },
-        { status: 401 }
-      );
-    }
-
-    const payload = jwt.decode(accessToken) as PayloadForUser;
-
-    if (payload.type !== "Administrador") {
-      return NextResponse.json(
-        { error: "No tiene permisos para realizar esta acción" },
-        { status: 403 }
-      );
-    }
-
-    const institution = await institutionsController.updateInstitution(request, id);
+    const institution = await institutionsController.updateInstitution(
+      request,
+      id
+    );
     return NextResponse.json(institution, { status: 200 });
   } catch (error) {
     if (
